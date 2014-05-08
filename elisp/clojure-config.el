703408eeeec6ed1c8e6f29f7b92aa30a85e8cdae
Michael Glaesemann <grzm@seespotcode.net>
2014-05-08 18:11:09 -0400

    Fix clojure indent

--- elisp/clojure-config.el
+++ elisp/clojure-config.el
@@ -1,1 +1,4 @@
 (add-hook 'clojure-mode-hook (lambda () (paredit-mode +1)))
+;; when breaking function calls in arguments, indent subsequent lines
+;; the default indent rather than align arguments
+(setq clojure-defun-style-default-indent t)
