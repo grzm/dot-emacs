ecbc0dd5d2269fcc7551adade9b5dee06ef87408
Michael Glaesemann <grzm@seespotcode.net>
2014-05-08 18:14:08 -0400

    Add ruby config

---
+++ elisp/ruby-config.el
@@ -0,0 +1,6 @@
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+
+
