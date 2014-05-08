8bc951a8afaae8785f20327ffde893eb6fd1eea0
Michael Glaesemann <grzm@seespotcode.net>
2014-05-08 18:13:42 -0400

    Add js config.

---
+++ elisp/js-config.el
@@ -0,0 +1,12 @@
+(require 'js-comint)
+(custom-set-variables '(js2-basic-offset 2)
+                      '(js2-bounce-indent-p 2))
+(setq inferior-js-program-command "/opt/local/bin/node")
+(setq inferior-js-mode-hook
+      (lambda ()
+        (ansi-color-for-comint-mode-on)
+        (add-to-list
+         'comint-preoutput-filter-functions
+         (lambda (output)
+           (replace-regexp-in-string "\033\\[[0-9]+[GK]" "" output)))))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
