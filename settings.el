0cb78efeb4b0ddf6258f2dd4982b608b9abb4f65
Michael Glaesemann <grzm@seespotcode.net>
2017-12-06 17:57:04 -0600

    Specify clojure test declaration
    Change-Id: I0ef78ffb9ec476fec71424490b9c1fde794ef52e

--- settings.el
+++ settings.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cljr-clojure-test-declaration "[clojure.test :as test :refer [are deftest is]]")
  '(custom-enabled-themes (quote (solarized-light)))
  '(custom-safe-themes
    (quote
