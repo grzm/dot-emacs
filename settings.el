4072da682190a47a155ab0c81f592e8a981d151f
Michael Glaesemann <grzm@seespotcode.net>
2020-03-23 21:00:37 -0500

    Update settings.

--- settings.el
+++ settings.el
@@ -11,7 +11,7 @@
  '(custom-enabled-themes (quote (solarized-light)))
  '(custom-safe-themes
    (quote
-    ("d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
+    ("c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
  '(js2-basic-offset 2)
  '(js2-bounce-indent-p 2)
  '(magit-diff-refine-hunk (quote all))
@@ -70,7 +70,7 @@
  '(org-trello-current-prefix-keybinding "C-c o")
  '(package-selected-packages
    (quote
-    (cider org-trello polymode go-mode ox-reveal magit org-projectile org-re-reveal transient dockerfile-mode docker-compose-mode sesman yaml-mode ripgrep projectile-ripgrep goto-last-change csv-mode dash-functional dot-mode org-bullets org-clubhouse quelpa-use-package projectile harvest timesheet let-alist inf-clojure markdown-mode markdown-preview-mode column-marker autopair solarized-theme which-key use-package typopunct typo sass-mode rainbow-delimiters php-mode graphviz-dot-mode gh-md clojure-mode-extra-font-locking)))
+    (org cider org-trello polymode go-mode ox-reveal magit org-projectile org-re-reveal transient dockerfile-mode docker-compose-mode sesman yaml-mode ripgrep projectile-ripgrep goto-last-change csv-mode dash-functional dot-mode org-bullets org-clubhouse quelpa-use-package projectile harvest timesheet let-alist inf-clojure markdown-mode markdown-preview-mode column-marker autopair solarized-theme which-key use-package typopunct typo sass-mode rainbow-delimiters php-mode graphviz-dot-mode gh-md clojure-mode-extra-font-locking)))
  '(safe-local-variable-values
    (quote
     ((cider-clojure-cli-global-options . "-A:dev -J-Dlogback.configurationFile=logback-dev.xml")
