1a908bd523315f34390d7b641ec21618931c9d11
Michael Glaesemann <grzm@seespotcode.net>
2020-04-02 22:16:09 -0500

    Ensure use-package is loaded.

--- init.el
+++ init.el
@@ -1,10 +1,25 @@
+;; ready, set, go!
 (defconst emacs-start-time (current-time))
 
 (unless noninteractive
   (message "Loading %s..." load-file-name))
 
+
+;; make sure we have use-package installed
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
 (package-initialize nil)
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; load the meat of it
 (require 'org)
 
 (org-babel-load-file (expand-file-name "common-config.org" user-emacs-directory))
@@ -15,6 +30,8 @@
       (viasat-dot-org (expand-file-name "README.org" viasat-emacs-directory)))
   (org-babel-load-file (if (file-exists-p viasat-dot-org) viasat-dot-org grzm-dot-org)))
 
+
+;; how long did this all take us?
 (when window-system
   (let ((elapsed (float-time (time-subtract (current-time)
                                             emacs-start-time))))
