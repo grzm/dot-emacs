19f6485efae2f82a679137b3280d440a9f3fa81e
Michael Glaesemann <grzm@seespotcode.net>
2020-04-02 20:31:56 -0500

    Definte viasat-emacs-directory variable.

--- init.el
+++ init.el
@@ -9,8 +9,10 @@
 
 (org-babel-load-file (expand-file-name "common-config.org" user-emacs-directory))
 
+(setq viasat-emacs-directory "~/.emacs.viasat.d/")
+
 (let ((grzm-org (expand-file-name "grzm.org" user-emacs-directory))
-      (viasat-org (expand-file-name "viasat.org" user-emacs-directory)))
+      (viasat-org (expand-file-name "viasat.org" viasat-emacs-directory)))
   (org-babel-load-file (if (file-exists-p viasat-org) viasat-org grzm-org)))
 
 (when window-system
