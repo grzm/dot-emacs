4fc354e859418c45e710147e03ab9e7973553852
Michael Glaesemann <grzm@seespotcode.net>
2020-04-04 04:12:18 -0500

    Bring it all under one roof.

--- init.el
+++ init.el
@@ -1,46 +1,14 @@
-;; ready, set, go!
-(defconst emacs-start-time (current-time))
+;; Just enough to get started.
+;; Lars Tveito's Emacs config is very helpful! https://github.com/larstvei/dot-emacs
 
-(unless noninteractive
-  (message "Loading %s..." load-file-name))
-
-
-;; make sure we have use-package installed
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
-
-(package-initialize nil)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(eval-when-compile
-  (require 'use-package))
-
-;; load the meat of it
+;; Tangling requires org.
 (require 'org)
 
-(org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
+;; Find the *real* configuration.
+(find-file (concat user-emacs-directory "init.org"))
 
-(setq viasat-emacs-directory "~/.emacs.viasat.d/")
+;; Tangle it.
+(org-babel-tangle)
 
-(let ((grzm-dot-org (expand-file-name "grzm.org" user-emacs-directory))
-      (viasat-dot-org (expand-file-name "README.org" viasat-emacs-directory)))
-  (org-babel-load-file (if (file-exists-p viasat-dot-org) viasat-dot-org grzm-dot-org)))
-
-
-;; how long did this all take us?
-(when window-system
-  (let ((elapsed (float-time (time-subtract (current-time)
-                                            emacs-start-time))))
-    (message "Loading %s...done (%.3fs)" load-file-name elapsed))
-
-  (add-hook 'after-init-hook
-            `(lambda ()
-               (let ((elapsed (float-time (time-subtract (current-time)
-                                                         emacs-start-time))))
-                 (message "Loading %s...done (%.3fs) [after-init]"
-                          ,load-file-name elapsed )))
-            t))
+;; Load it.
+(load-file (concat user-emacs-directory "init.el"))
