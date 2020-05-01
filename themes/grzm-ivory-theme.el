eed55fdd7d9262086c0274622e8269c6cf818ceb
Michael Glaesemann <grzm@seespotcode.net>
2020-05-01 08:29:49 -0500

    Additional face tweaks for ivory.

--- themes/grzm-ivory-theme.el
+++ themes/grzm-ivory-theme.el
@@ -1,13 +1,31 @@
 (deftheme grzm-ivory
   "A light Emacs theme.")
 
+(setq ivory-background "#fefefe")
+(setq ivory-base "black")
+(setq ivory-gray "gray")
+
+;; https://coolors.co/e63946-f1faee-a8dadc-457b9d-1d3557
+(setq deep-red "#720026")
+(setq deep-red-2 "#96031a")
+(setq imperial-red "#e63946")
+(setq light-red "#ec9a9a")
+(setq honeydew "#f1faee")
+(setq powder-blue "#a8dadc")
+(setq steel-blue "#457b9d")
+(setq prussian-blue "#1d3557")
+
 (custom-theme-set-faces
  'grzm-ivory
- '(default ((t (:background "#fefefe" :foreground "#000"))))
- 
+ `(default ((t (:background ,ivory-background :foreground ,ivory-base))))
+ `(secondary-selection ((t (:background ,ivory-background :foreground ,ivory-base))))
+
  '(Info-quoted ((t (:underline "gray40" :weight bold))))
  '(aw-leading-char-face ((t (:background "red" :foreground "white" :weight bold))))
- '(default ((t (:background "white" :foreground "black"))))
+ `(default ((t (:background ,ivory-background :foreground ,ivory-base))))
+
+ `(clojure-keyword-face ((t (:foreground ,prussian-blue :weight bold))))
+
  '(diff-added ((t (:inherit diff-changed :background "light green"))))
  '(diff-changed ((t (:background "light steel blue"))))
  '(diff-indicator-added ((t (:inherit diff-indicator-changed))))
@@ -16,18 +34,19 @@
  '(diff-removed ((t (:inherit diff-changed :background "sandy brown"))))
  '(dired-directory ((t (:inherit font-lock-function-name-face :weight bold))))
 
- '(font-lock-builtin-face ((t (:foreground "black"))))
- '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
- '(font-lock-comment-face ((t (:foreground "black"))))
- '(font-lock-doc-face ((t (:inherit font-lock-string-face :slant italic))))
- '(font-lock-function-name-face ((t (:foreground "black"))))
- '(font-lock-keyword-face ((t (:foreground "black"))))
+ `(font-lock-builtin-face ((t (:foreground ,ivory-base))))
+ '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face :foreground "#aaaaaa" :slant italic))))
+ `(font-lock-comment-face ((t (:foreground ,ivory-base :background "#eeeeee"))))
+ `(font-lock-constant-face ((t (:foreground ,prussian-blue))))
+ '(font-lock-doc-face ((t (:inherit font-lock-string-face :slant italic :background "#eeeeee"))))
+ `(font-lock-function-name-face ((t (:foreground ,ivory-base :weight bold))))
+ `(font-lock-keyword-face ((t (:foreground ,ivory-base))))
  '(font-lock-negation-char-face ((t (:weight bold))))
- '(font-lock-regexp-grouping-backslash ((t (:inherit bold :foreground "black"))))
- '(font-lock-regexp-grouping-construct ((t (:inherit bold :foreground "black"))))
- '(font-lock-string-face ((t (:foreground "black"))))
- '(font-lock-type-face ((t (:foreground "black"))))
- '(font-lock-variable-name-face ((t (:foreground "black"))))
+ `(font-lock-regexp-grouping-backslash ((t (:inherit bold :foreground ,ivory-base))))
+ `(font-lock-regexp-grouping-construct ((t (:inherit bold :foreground ,ivory-base))))
+ `(font-lock-string-face ((t (:foreground ,steel-blue :weight bold))))
+ `(font-lock-type-face ((t (:foreground ,ivory-base :weight bold))))
+ `(font-lock-variable-name-face ((t (:foreground ,ivory-base))))
 
  '(gnus-button ((t (:inherit button))))
  '(gnus-header-name ((t (:box (:line-width 1 :style released-button) :weight bold))))
@@ -46,21 +65,25 @@
  '(hl-line ((t (:background "#f0f0f1"))))
  '(hl-paren-face ((t (:weight bold))) t)
  '(minibuffer-prompt ((t (:foreground "#0184bc" :box (:line-width -1 :style released-button) :weight bold))))
- '(mode-line ((t (:background "#f0f0f1" :box (:line-width 1 :color "#383a42")))))
+ '(mode-line ((t (:background "#f0f0f1"))))
  '(mode-line-inactive ((t (:inherit mode-line :foreground "#a0a1a7"))))
+
  '(org-agenda-date ((t (:inherit org-agenda-structure))))
  '(org-agenda-date-today ((t (:inherit org-agenda-date :underline t))))
  '(org-agenda-date-weekend ((t (:inherit org-agenda-date :foreground "dark green"))))
  '(org-agenda-structure ((t (:foreground "Blue1" :weight bold :height 1.1 :family "DeJaVu Sans"))))
- 
+
+ `(org-agenda-date ((t (:inherit fixed-pitch))))
  '(org-default ((t (:inherit variable-pitch))))
- '(org-block-begin-line ((t (:inherit org-meta-line :foreground "black" :underline t))))
- '(org-block-end-line ((t (:inherit org-block-begin-line :overline t))))
- '(org-document-info ((t (:foreground "black"))))
- '(org-document-info-keyword ((t (:foreground "black"))))
- '(org-document-title ((t (:foreground "black" :weight bold))))
- '(org-ellipsis ((t (:foreground "gray" :weight normal))))
- '(org-hide ((t (:foreground "white"))))
+ `(org-block ((t (:inherit org-meta-line :foreground ,ivory-base :height 0.9))))
+ `(org-block-begin-line ((t (:inherit org-meta-line :foreground ,ivory-gray :underline t :height 0.9))))
+ '(org-block-end-line ((t (:inherit org-block-begin-line :overline t :underline nil))))
+ `(org-date ((t (:inherit fixed-pitch))))
+ `(org-document-info ((t (:foreground ,ivory-base))))
+ `(org-document-info-keyword ((t (:inherit fixed-pitch :foreground ,ivory-gray :weight normal))))
+ `(org-document-title ((t (:foreground ,ivory-base :weight bold))))
+ `(org-ellipsis ((t (:foreground ,ivory-gray :weight normal))))
+ `(org-hide ((t (:foreground ,ivory-background))))
  '(org-level-1 ((t (:inherit outline-1 :box nil))))
  '(org-level-2 ((t (:inherit outline-2 :box nil))))
  '(org-level-3 ((t (:inherit outline-3 :box nil))))
@@ -69,9 +92,12 @@
  '(org-level-6 ((t (:inherit outline-6 :box nil))))
  '(org-level-7 ((t (:inherit outline-7 :box nil))))
  '(org-level-8 ((t (:inherit outline-8 :box nil))))
- '(org-special-keyword ((t (:inherit font-lock-keyword-face :foreground "gray"))))
+ `(org-meta-line ((t (:inherit org-document-info-keyword :foreground ,ivory-gray :height 0.9))))
+ `(org-special-keyword ((t (:inherit font-lock-keyword-face :foreground ,ivory-gray))))
+ `(org-verbatim ((t (:inherit fixed-pitch :foreground ,ivory-base :weight normal))))
+ `(org-table ((t (:inherit fixed-pitch))))
  '(org-tag ((t (:weight bold))))
- 
+
  '(outline-1 ((t (:inherit font-lock-function-name-face :weight bold))))
  '(outline-2 ((t (:inherit font-lock-variable-name-face :weight bold))))
  '(outline-3 ((t (:inherit font-lock-keyword-face :weight bold))))
@@ -92,8 +118,8 @@
  '(realgud-line-number           ((t :foreground "#0184bc")))
  '(realgud-backtrace-number      ((t :foreground "#0184bc" :weight bold)))
  '(region ((t (:background "lightgoldenrod1"))))
- '(show-paren-match ((t (:background "Cyan1" :weight bold))))
- '(show-paren-mismatch ((t (:background "deep pink" :weight bold))))
+ `(show-paren-match ((t (:foreground ,ivory-base :weight bold))))
+ '(show-paren-mismatch ((t (:background "red" :weight bold))))
  '(window-number-face ((t (:foreground "red" :weight bold)))))
 
 ;;;###autoload
